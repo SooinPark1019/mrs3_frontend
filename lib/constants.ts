a2db686d69107c14b7a0d383697d344e5e9d9273
prima <102790490+primarina314@users.noreply.github.com>
2025-08-20 17:18:37 +0900

    Update constants.ts

--- lib/constants.ts
+++ lib/constants.ts
@@ -3,7 +3,7 @@
  * - 프론트 전역에서 재사용되는 API URL, 엔드포인트, 선택 옵션 등을 제공합니다.
  * - 환경 변수: NEXT_PUBLIC_API_BASE_URL (예: http://localhost:8000)
  */
-export const API_BASE_URL =  "https://bd16de4d1a27.ngrok-free.app"
+export const API_BASE_URL =  "https://b2993d072a12.ngrok-free.app"
 
 // 백엔드 엔드포인트
 export const ENDPOINTS = {
